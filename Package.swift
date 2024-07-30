9567ad21349c892180c901fdd40efd4ed5a86119
sookim-1 <scstnghks@gmail.com>
2024-07-30 15:43:59 +0900

    [Fixed] : Test 타겟 제거

--- Package.swift
+++ Package.swift
@@ -13,10 +13,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/daltoniam/Starscream.git", .exact("3.1.1")),
-        .package(url: "https://github.com/alibaba/HandyJSON.git", .exact("5.0.1")),
-        .package(url: "https://github.com/Quick/Quick.git", .exact("2.2.0")),
-        .package(url: "https://github.com/Quick/Nimble.git", .exact("8.0.2")),
-        .package(url: "https://github.com/ReactiveX/RxSwift.git", from: "5.0.0")
+        .package(url: "https://github.com/alibaba/HandyJSON.git", .exact("5.0.1"))
         ],
     targets: [
         .target(
@@ -29,19 +26,5 @@ let package = Package(
             name: "Main",
             dependencies: [
                 "ScClient",
-                ]),
-        .testTarget(
-            name: "Integration",
-            dependencies: [
-                "ScClient",
-                "Quick",
-                "Nimble",
-                "RxSwift"
-                ]),
-        .testTarget(
-            name: "Unit",
-            dependencies: [
-                "ScClient"
-                ])
-        ]
+                ])        ]
 )
