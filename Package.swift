874e2afd838e072b182c57cd7152b44b1708a1bc
김수환 <scstnghks.papamobility@gamil.com>
2024-09-26 19:20:21 +0900

    [Fixed] : HandyJSON

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/daltoniam/Starscream.git", .exact("3.1.1")),
-        .package(url: "https://github.com/alibaba/HandyJSON.git", .exact("5.0.1"))
+        .package(url: "https://github.com/Miles-Matheson/HandyJSON.git", .branch("master"))
         ],
     targets: [
         .target(
